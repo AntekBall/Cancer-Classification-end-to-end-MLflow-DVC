7bb97897ab15a7da10bf2da42155545681d4b9fd
Antek <antekballaun96@gmail.com>
2025-07-24 13:49:25 +0200

    model trainer

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from ChestCancerCNN import logger
 from ChestCancerCNN.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from ChestCancerCNN.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from ChestCancerCNN.pipeline.stage_03_model_trainer import ModelTrainingPipeline
+
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -25,3 +27,16 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+STAGE_NAME = "Training"
+
+if __name__ == '__main__':
+    try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
